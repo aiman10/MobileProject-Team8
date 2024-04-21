66080174db12cb68227e02308d978e3ab8a15c6c
aiman10 <37143774+aiman10@users.noreply.github.com>
2024-04-21 16:41:07 +0300

    Limit api calls

--- services/Currency.js
+++ services/Currency.js
@@ -1,16 +1,35 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const fetchCurrencies = async () => {
+  const cacheKey = "currenciesData";
+  const lastFetchKey = "lastFetchCurrencies";
+
   try {
+    const lastFetch = await AsyncStorage.getItem(lastFetchKey);
+    const isDataOld =
+      Date.now() - new Date(lastFetch).getTime() > 24 * 60 * 60 * 1000;
+
+    if (lastFetch && !isDataOld) {
+      const cachedData = await AsyncStorage.getItem(cacheKey);
+      if (cachedData) {
+        return JSON.parse(cachedData);
+      }
+    }
+
     const response = await axios.get(
-      "https://v6.exchangerate-api.com/v6/649f8dae2e49a134b663d0f6/codes"
+      "https://v6.exchangerate-api.com/v6/1b9fae45b46e8783d8a00c5b/codes"
     );
     if (response.data && response.data.supported_codes) {
       const currencyData = response.data.supported_codes.map((code) => ({
         label: `${code[0]} - ${code[1]}`,
-        //label: `${code[0]}`,
         value: code[0],
       }));
+
+      // Update cache
+      await AsyncStorage.setItem(cacheKey, JSON.stringify(currencyData));
+      await AsyncStorage.setItem(lastFetchKey, new Date().toISOString());
+
       return currencyData;
     }
   } catch (error) {
@@ -23,7 +42,7 @@ export const fetchConversionRate = async (fromCurrency, toCurrency) => {
   // console.log("toCurrency", toCurrency);
   try {
     const response = await axios.get(
-      `https://v6.exchangerate-api.com/v6/649f8dae2e49a134b663d0f6/pair/${fromCurrency}/${toCurrency}`
+      `https://v6.exchangerate-api.com/v6/1b9fae45b46e8783d8a00c5b/pair/${fromCurrency}/${toCurrency}`
     );
     if (response.data && response.data.conversion_rate) {
       return response.data.conversion_rate;
